5e4156e4f93e7b1b4da330bb7d9cc8f73105c2e7
Clement <clement_392@hotmail.com>
2021-11-14 20:06:13 +0800

    New template

--- header.php
+++
@@ -1,4 +0,0 @@
-<head>
-	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
-</head>
--- nav.php
+++
@@ -1,6 +0,0 @@
-<nav class="navbar navbar-default" 
-	style="background-color: green;">
-	<div class="container-fluid">
-		<a class="navbar-brand" href="./index.php" style="color: white;">IR-Acer Web Console</a>
-	</div>
-</nav>
