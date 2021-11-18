0fa6bd395f317646d4aa69a0945fd96212867983
James Lim <jaystersnsd@gmail.com>
2021-11-18 14:05:53 +0800

    nav changes

--- common_resources/sidebartest.php
+++ common_resources/sidebartest.php
@@ -6,7 +6,7 @@
 	<!-- Sidebar - Brand -->
 	<a
 		class="sidebar-brand d-flex align-items-center justify-content-center"
-		href="index.php">
+		href="index.html">
 		<div class="sidebar-brand-icon rotate-n-15">
 			<i class="fas fa-laugh-wink"></i>
 		</div>
@@ -17,7 +17,7 @@
 	<hr class="sidebar-divider my-0">
 
 	<!-- Nav Item - Dashboard -->
-	<li class="nav-item"><a class="nav-link" href="index.php"> <i
+	<li class="nav-item"><a class="nav-link" href="index.html"> <i
 			class="fas fa-fw fa-tachometer-alt"></i> <span>Dashboard</span></a></li>
 
 	<!-- Divider -->
@@ -29,22 +29,33 @@
 	<!-- Nav Item - Pages Collapse Menu -->
 	<li class="nav-item"><a class="nav-link collapsed" href="#"
 		data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-		aria-controls="collapseTwo"> <i class="fas fa-fw fa-cog"></i> <span>Game Settings</span>
+		aria-controls="collapseTwo"> <i class="fas fa-fw fa-cog"></i> <span>Components</span>
 	</a>
 		<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
 			data-parent="#accordionSidebar">
 			<div class="bg-white py-2 collapse-inner rounded">
 				<h6 class="collapse-header">Custom Components:</h6>
-				<a class="collapse-item" href="gamediff.html">Difficulty</a> <a
-					class="collapse-item" href="cards.html">Tutorial</a>
+				<a class="collapse-item" href="buttons.html">Buttons</a> <a
+					class="collapse-item" href="cards.html">Cards</a>
 			</div>
 		</div></li>
 
 	<!-- Nav Item - Utilities Collapse Menu -->
-	<li class="nav-item"><a class="nav-link collapsed" href="../../challenges.php"> <i
-			class="fas fa-fw fa-wrench"></i> <span>Challenges</span>
+	<li class="nav-item"><a class="nav-link collapsed" href="#"
+		data-toggle="collapse" data-target="#collapseUtilities"
+		aria-expanded="true" aria-controls="collapseUtilities"> <i
+			class="fas fa-fw fa-wrench"></i> <span>Utilities</span>
 	</a>
-		</li>
+		<div id="collapseUtilities" class="collapse"
+			aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+			<div class="bg-white py-2 collapse-inner rounded">
+				<h6 class="collapse-header">Custom Utilities:</h6>
+				<a class="collapse-item" href="utilities-color.html">Colors</a> <a
+					class="collapse-item" href="utilities-border.html">Borders</a> <a
+					class="collapse-item" href="utilities-animation.html">Animations</a>
+				<a class="collapse-item" href="utilities-other.html">Other</a>
+			</div>
+		</div></li>
 
 	<!-- Divider -->
 	<hr class="sidebar-divider">
@@ -73,11 +84,11 @@
 		</div></li>
 
 	<!-- Nav Item - Charts -->
-	<li class="nav-item"><a class="nav-link" href="charts.php"> <i
+	<li class="nav-item"><a class="nav-link" href="charts.html"> <i
 			class="fas fa-fw fa-chart-area"></i> <span>Charts</span></a></li>
 
 	<!-- Nav Item - Tables -->
-	<li class="nav-item"><a class="nav-link" href="tables.php"> <i
+	<li class="nav-item"><a class="nav-link" href="tables.html"> <i
 			class="fas fa-fw fa-table"></i> <span>Tables</span></a></li>
 
 	<!-- Divider -->
