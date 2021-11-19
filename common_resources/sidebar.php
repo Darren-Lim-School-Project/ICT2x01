2ee3df6d93be33c857dbdae69ff3a876457ba896
James Lim <jaystersnsd@gmail.com>
2021-11-19 16:53:20 +0800

    new

--- common_resources/sidebar.php
+++ common_resources/sidebar.php
@@ -10,7 +10,7 @@
 		<div class="sidebar-brand-icon rotate-n-15">
 			<i class="fas fa-laugh-wink"></i>
 		</div>
-		<div class="sidebar-brand-text mx-3">IR-Acer Admin Panel</div>
+		<div class="sidebar-brand-text mx-3">IR-Acer User Panel</div>
 	</a>
 
 	<!-- Divider -->
@@ -24,61 +24,23 @@
 	<hr class="sidebar-divider">
 
 	<!-- Heading -->
-	<div class="sidebar-heading">Interface</div>
+	<div class="sidebar-heading">User Menu</div>
 
 	<!-- Nav Item - Pages Collapse Menu -->
-	<li class="nav-item"><a class="nav-link collapsed" href="#"
-		data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-		aria-controls="collapseTwo"> <i class="fas fa-fw fa-cog"></i> <span>Game Settings</span>
-	</a>
-		<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
-			data-parent="#accordionSidebar">
-			<div class="bg-white py-2 collapse-inner rounded">
-				<h6 class="collapse-header">Custom Components:</h6>
-				<a class="collapse-item" href="gamediff.html">Difficulty</a> <a
-					class="collapse-item" href="cards.html">Tutorial</a>
-			</div>
-		</div></li>
 
-	<!-- Nav Item - Utilities Collapse Menu -->
-	<li class="nav-item"><a class="nav-link collapsed" href="../../challenges.php"> <i
-			class="fas fa-fw fa-wrench"></i> <span>Challenges</span>
+        <li class="nav-item"><a class="nav-link collapsed" href="../../challenges.php"> <span>Play Game</span>
 	</a>
+        <li class="nav-item"><a class="nav-link collapsed" href="tutorial.php"> <span>Tutorial</span>
+	</a>
+        <li class="nav-item"><a class="nav-link collapsed" href="leaderboard.php"> <span>Leaderboard</span>
+	</a>
+        <li class="nav-item">
 		</li>
 
-	<!-- Divider -->
-	<hr class="sidebar-divider">
+<!--	 Divider 
+	<hr class="sidebar-divider">-->
 
-	<!-- Heading -->
-	<div class="sidebar-heading">Addons</div>
-
-	<!-- Nav Item - Pages Collapse Menu -->
-	<li class="nav-item active"><a class="nav-link" href="#"
-		data-toggle="collapse" data-target="#collapsePages"
-		aria-expanded="true" aria-controls="collapsePages"> <i
-			class="fas fa-fw fa-folder"></i> <span>Pages</span>
-	</a>
-		<div id="collapsePages" class="collapse"
-			aria-labelledby="headingPages" data-parent="#accordionSidebar">
-			<div class="bg-white py-2 collapse-inner rounded">
-				<h6 class="collapse-header">Login Screens:</h6>
-				<a class="collapse-item" href="login.html">Login</a> <a
-					class="collapse-item" href="register.html">Register</a> <a
-					class="collapse-item" href="forgot-password.html">Forgot Password</a>
-				<div class="collapse-divider"></div>
-				<h6 class="collapse-header">Other Pages:</h6>
-				<a class="collapse-item" href="404.html">404 Page</a> <a
-					class="collapse-item active" href="blank.html">Blank Page</a>
-			</div>
-		</div></li>
-
-	<!-- Nav Item - Charts -->
-	<li class="nav-item"><a class="nav-link" href="charts.php"> <i
-			class="fas fa-fw fa-chart-area"></i> <span>Charts</span></a></li>
-
-	<!-- Nav Item - Tables -->
-	<li class="nav-item"><a class="nav-link" href="tables.php"> <i
-			class="fas fa-fw fa-table"></i> <span>Tables</span></a></li>
+	
 
 	<!-- Divider -->
 	<hr class="sidebar-divider d-none d-md-block">
